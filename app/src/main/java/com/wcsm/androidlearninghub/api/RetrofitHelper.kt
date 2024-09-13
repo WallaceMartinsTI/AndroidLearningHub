d5ce5516172b8d55c037ca7b581edc9ad1fd76c5
Wallace Martins <wallace159santos@hotmail.com>
2024-09-12 22:54:05 -0300

    start configs of retrofit

---
+++ app/src/main/java/com/wcsm/androidlearninghub/api/RetrofitHelper.kt
@@ -0,0 +1,15 @@
+package com.wcsm.androidlearninghub.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitHelper {
+
+    companion object {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
