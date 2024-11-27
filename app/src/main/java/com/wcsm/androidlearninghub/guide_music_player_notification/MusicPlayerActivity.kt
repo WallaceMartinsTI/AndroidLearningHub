3508fbd46af02cc54db8f86de871702edb74f122
Wallace Martins <wallace159santos@hotmail.com>
2024-11-26 22:12:36 -0300

    starting 'Music Player Notification'

---
+++ app/src/main/java/com/wcsm/androidlearninghub/guide_music_player_notification/MusicPlayerActivity.kt
@@ -0,0 +1,21 @@
+package com.wcsm.androidlearninghub.guide_music_player_notification
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.wcsm.androidlearninghub.R
+
+class MusicPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_music_player)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
